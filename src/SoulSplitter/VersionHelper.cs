02988e676f2f705aa4fb8311d9032033e3103770
Frank van der Stam <frank_stam5@hotmail.com>
2022-05-04 19:09:01 +0200

    Bump version

--- src/SoulSplitter/VersionHelper.cs
+++ src/SoulSplitter/VersionHelper.cs
@@ -8,6 +8,6 @@ namespace SoulSplitter
 {
     internal static class VersionHelper
     {
-        public static Version Version => new Version(0, 0, 20);
+        public static Version Version => new Version(0, 0, 21);
     }
 }
