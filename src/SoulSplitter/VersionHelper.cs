0b6cceb4a12332715340140a600b91d4df397c48
Frank van der Stam <frank_stam5@hotmail.com>
2022-08-26 09:05:17 +0200

    1.1.4

--- src/SoulSplitter/VersionHelper.cs
+++ src/SoulSplitter/VersionHelper.cs
@@ -8,6 +8,6 @@ namespace SoulSplitter
 {
     internal static class VersionHelper
     {
-        public static Version Version => new Version(1, 1, 3);
+        public static Version Version => new Version(1, 1, 4);
     }
 }
