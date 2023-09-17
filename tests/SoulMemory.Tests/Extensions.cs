fa9ce7544f55f0b85931aa066b4e602ee7f81dc8
Frank <frank_stam5@hotmail.com>
2023-09-17 17:07:20 +0200

    Impl tests for error types (#20)

---
+++ tests/SoulMemory.Tests/Extensions.cs
@@ -0,0 +1,35 @@
+﻿// This file is part of the SoulSplitter distribution (https://github.com/FrankvdStam/SoulSplitter).
+// Copyright (c) 2022 Frank van der Stam.
+// https://github.com/FrankvdStam/SoulSplitter/blob/main/LICENSE
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoulMemory.Tests
+{
+    public static class Extensions
+    {
+        public static void DoesNotThrow(this Assert assert, Action a)
+        {
+            try
+            {
+                a();
+            }
+            catch (Exception e)
+            {
+                throw new AssertFailedException(e.ToString());
+            }
+        }
+    }
+}
