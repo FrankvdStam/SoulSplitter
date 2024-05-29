8e9e897f1e2e67d8df8339d484f2f3c387ec69d2
Frank <frank_stam5@hotmail.com>
2024-05-29 18:40:42 +0200

    Feature/soulmods build target (#37)
    * Update license headers in soulmods. Fix x86 build for soulmods. SoulMemory now builds soulmods automagically.
    * Remove soulmods.dll since it's tied into the build now.
    * Fix missing x64 directory
    * Remove explicit soulmods build from pipeline build since it's tied to the .NET build now.

--- src/SoulMemory/soulmods/Soulmods.cs
+++ src/SoulMemory/soulmods/Soulmods.cs
@@ -15,11 +15,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using SoulMemory.Native;
 
 namespace SoulMemory.soulmods
