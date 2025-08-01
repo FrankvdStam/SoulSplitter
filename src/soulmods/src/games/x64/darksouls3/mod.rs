577624260a7e33c190ba5d6883f73c8b44ea8cfe
Frank van der Stam <frank_stam5@hotmail.com>
2025-08-01 21:26:01 +0200

    Reworked from feature/soulmods-wip

--- src/soulmods/src/games/x64/darksouls3/mod.rs
+++ src/soulmods/src/games/x64/darksouls3/mod.rs
@@ -27,7 +27,6 @@ use crate::games::x64::darksouls3::migt::ds3_init_migt;
 use crate::util::*;
 
 static mut FPS_HOOK: Option<HookPoint> = None;
-static mut FPS_HISTORY_HOOK: Option<HookPoint> = None;
 static mut FPS_CUSTOM_LIMIT_HOOK: Option<HookPoint> = None;
 static mut FRAME_ADVANCE_HOOK: Option<HookPoint> = None;
 
@@ -55,132 +54,167 @@ pub static mut DS3_FRAME_RUNNING: bool = false;
 pub fn init_darksouls3()
 {
     unsafe
-        {
-            info!("version: {}", GLOBAL_VERSION);
+    {
+        info!("version: {}", GLOBAL_VERSION);
 
-            // Get DS3 process
-            let mut process = Process::new_with_memory_type("darksoulsiii.exe", MemoryType::Direct);
-            process.refresh().unwrap();
+        // Get DS3 process
+        let mut process = Process::new_with_memory_type("darksoulsiii.exe", MemoryType::Direct);
+        process.refresh().unwrap();
 
-            ds3_init_migt(&process);
-            
-            
-            // AoB scan for FPS patch
-            let fn_fps_address = process.scan_abs("fps", "f3 0f 58 93 64 02 00 00 41 0f 2f d4", 0, Vec::new()).unwrap().get_base_address();
-            info!("FPS at 0x{:x}", fn_fps_address);
+        ds3_init_migt(&process);
 
-            // Enable FPS patch
-            FPS_HOOK = Some(Hooker::new(fn_fps_address, HookType::JmpBack(fps), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+        // AoB scan for FPS patch
+        let fn_fps_address = process.scan_abs("fps", "f3 0f 10 88 68 02 00 00 48 8d 3d", 0, Vec::new()).unwrap().get_base_address();
+        info!("FPS at 0x{:x}", fn_fps_address);
 
+        // AoB scan for FPS custom limit patch
+        let fn_fps_custom_limit_address = process.scan_abs("fps custom limit", "48 8b c8 e8 ? ? ? ? 48 8b 05 ? ? ? ? 48 85 c0 75 26 4c 8d 0d ? ? ? ? 4c 8d 05 ? ? ? ? ba b1 00 00 00 48 8d 0d ? ? ? ? e8 ? ? ? ? 48 8b 05 ? ? ? ? f3 0f 10 88 68 02 00 00 48 8d 3d", 0, Vec::new()).unwrap().get_base_address();
+        info!("FPS custom limit at 0x{:x}", fn_fps_custom_limit_address);
 
-            // AoB scan for FPS history patch
-            let fn_fps_history_address = process.scan_abs("fps history", "48 89 04 cb 0f b6 83 78 02 00 00", 0, Vec::new()).unwrap().get_base_address();
-            info!("FPS history at 0x{:x}", fn_fps_history_address);
-
-            // Enable FPS history patch
-            FPS_HISTORY_HOOK = Some(Hooker::new(fn_fps_history_address, HookType::JmpBack(fps_history), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+        // AoB scan for frame advance patch
+        let fn_frame_advance_address = process.scan_abs("frame_advance", "e8 ? ? ? ? 84 c0 74 41 90 48", 10, Vec::new()).unwrap().get_base_address();
+        info!("Frame advance at 0x{:x}", fn_frame_advance_address);
 
 
-            // AoB scan for FPS custom limit patch
-            let fn_fps_custom_limit_address = process.scan_abs("fps custom limit", "f3 44 0f 10 2d ? ? ? ? 44 38 a3 58 03 00 00", 0, Vec::new()).unwrap().get_base_address();
-            info!("FPS custom limit at 0x{:x}", fn_fps_custom_limit_address);
-
-            // Enable FPS custom limit patch
-            FPS_CUSTOM_LIMIT_HOOK = Some(Hooker::new(fn_fps_custom_limit_address, HookType::JmpBack(fps_custom_limit), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+        // Enable frame advance patch
+        FRAME_ADVANCE_HOOK = Some(Hooker::new(fn_frame_advance_address, HookType::JmpBack(frame_advance), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
 
 
-            // AoB scan for frame advance patch
-            let fn_frame_advance_address = process.scan_abs("frame_advance", "e8 ? ? ? ? 84 c0 74 41 90 48", 10, Vec::new()).unwrap().get_base_address();
-            info!("Frame advance at 0x{:x}", fn_frame_advance_address);
+        // Temporarily turn on frame-advance while patching the FPS function..
+        DS3_FRAME_ADVANCE_ENABLED = true;
+        DS3_FRAME_RUNNING = true;
 
-            // Enable frame advance patch
-            FRAME_ADVANCE_HOOK = Some(Hooker::new(fn_frame_advance_address, HookType::JmpBack(frame_advance), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+        while DS3_FRAME_RUNNING {
+            thread::sleep(Duration::from_micros(10));
         }
+
+
+        // Enable FPS patch
+        FPS_HOOK = Some(Hooker::new(fn_fps_address, HookType::JmpBack(fps), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+
+        // Enable FPS custom limit patch
+        FPS_CUSTOM_LIMIT_HOOK = Some(Hooker::new(fn_fps_custom_limit_address, HookType::JmpBack(fps_custom_limit), CallbackOption::None, 0, HookFlags::empty()).hook().unwrap());
+
+
+        // Turn off frame-advance again..
+        DS3_FRAME_ADVANCE_ENABLED = false;
+    }
 }
 
 // FPS patch
-// Sets the calculated frame delta to always be the target frame delta.
-// It also sets the previous frames timestamp to be the current one minus the target frame delta.
-// This makes it so the game always behaves as if it's running at the FPS limit, with slowdowns if the PC can't keep up.
-// A second patch, "FPS history" below, is required in addition to this one to ensure accuracy.
 unsafe extern "win64" fn fps(registers: *mut Registers, _:usize)
 {
     unsafe
     {
         if DS3_FPS_PATCH_ENABLED
         {
-            let ptr_flipper = (*registers).rbx as *const u8; // Flipper struct - Contains all the stuff we need
+            // Flipper struct: Contains all the stuff we need
+            let ptr_flipper = (*registers).rax as *const u8;
 
-            let ptr_target_frame_delta = ptr_flipper.offset(0x18) as *mut f32; // Target frame delta - Set in a switch/case at the start
-            let ptr_timestamp_previous = ptr_flipper.offset(0x20) as *mut u64; // Previous frames timestamp
-            let ptr_timestamp_current = ptr_flipper.offset(0x28) as *mut u64; // Current frames timestamp
-            let ptr_frame_delta = ptr_flipper.offset(0x264) as *mut f32; // Current frames frame delta
-            let ptr_frame_delta_copy = ptr_flipper.offset(0x2b8) as *mut f32; // Current frames frame delta - Copy that's assigned differently than in ER, so we assign it manually too
 
-            // Read target frame data, the current timestamp and then calculate the timestamp diff at stable FPS
-            let target_frame_delta = std::ptr::read_volatile(ptr_target_frame_delta);
+            // Prepare some frametime values that will be used a few times
+            let ptr_frametime = ptr_flipper.offset(0x18) as *mut f32;
+            let frametime = std::ptr::read_volatile(ptr_frametime);
+            let frametime_int = (frametime * 10000000.0) as i32;
+            let frametime_truncated = (frametime_int as f32) / 10000000.0;
+            let frametime_cursed = f32::from_bits(frametime_truncated.to_bits() + 0x1); // Idk why, but somehow that's needed.. Thanks fromsoft.
+
+
+            // Timestamp values
+            // Just grab the current one and set the previous one to that - target frametime
+            let ptr_timestamp_previous = ptr_flipper.offset(0x20) as *mut u64;
+            let ptr_timestamp_current = ptr_flipper.offset(0x28) as *mut u64;
+
             let timestamp_current = std::ptr::read_volatile(ptr_timestamp_current);
-            let timestamp_diff = (target_frame_delta * 10000000.0) as i32;
+            std::ptr::write_volatile(ptr_timestamp_previous, timestamp_current - (frametime_int as u64));
 
-            // Calculate the previous timestamp, as well as the frame delta
-            let timestamp_previous = timestamp_current - (timestamp_diff as u64);
-            let frame_delta = (timestamp_diff as f32) / 10000000.0;
-
-            // Write values back
-            std::ptr::write_volatile(ptr_timestamp_previous, timestamp_previous);
-            std::ptr::write_volatile(ptr_frame_delta, frame_delta);
-            std::ptr::write_volatile(ptr_frame_delta_copy, frame_delta);
-        }
-    }
-}
+            // A bunch of unknown timestamps that are somewhat close to the current timestamp..
+            // ..so just set them to the current timestamp and hope it works out?
+            for n in 0..6 {
+                let ptr_timestamp_unknown = ptr_flipper.offset(0x30 + n * 0x8) as *mut u64;
+                std::ptr::write_volatile(ptr_timestamp_unknown, timestamp_current);
+            }
 
 
-// FPS history patch
-// Similar to the FPS patch, this sets the difference between the previous and current frames timestamps to the target frame delta.
-// This gets stored in an array with 32 elements, possibly for calculating FPS averages.
-unsafe extern "win64" fn fps_history(registers: *mut Registers, _:usize)
-{
-    unsafe
-    {
-        if DS3_FPS_PATCH_ENABLED
-        {
-            let ptr_flipper = (*registers).rbx as *const u8; // Flipper struct - Contains all the stuff we need
+            // Frametime history
+            for n in 0..32 {
+                let ptr_frametime_history = ptr_flipper.offset(0x60 + n * 0x10) as *mut u64;
+                std::ptr::write_volatile(ptr_frametime_history, frametime_int as u64);
+            }
 
-            let ptr_target_frame_delta = ptr_flipper.offset(0x18) as *mut f32; // Target frame delta - Set in a switch/case at the start
+            // FPS
+            let ptr_fps = ptr_flipper.offset(0x2bc) as *mut f32;
+            std::ptr::write_volatile(ptr_fps, 1.0f32 / frametime_cursed);
 
-            // Read the target frame delta and write back the calculated frame delta timestamp
-            let target_frame_delta = std::ptr::read_volatile(ptr_target_frame_delta);
-            (*registers).rax = (target_frame_delta * 10000000.0) as u64;
+
+            // Frametimes..
+            let ptr_frametime_0x264 = ptr_flipper.offset(0x264) as *mut f32;
+            let ptr_frametime_0x268 = ptr_flipper.offset(0x268) as *mut f32;
+            let ptr_frametime_0x26c = ptr_flipper.offset(0x26c) as *mut f32;
+            std::ptr::write_volatile(ptr_frametime_0x264, frametime_truncated);
+            std::ptr::write_volatile(ptr_frametime_0x268, frametime_truncated);
+            std::ptr::write_volatile(ptr_frametime_0x26c, frametime_cursed);
+
+            // ..more frametimes..
+            for n in 0..16 {
+                let ptr_frametime_unknown = ptr_flipper.offset(0x27c + n * 0x4) as *mut f32;
+                std::ptr::write_volatile(ptr_frametime_unknown, frametime_truncated);
+            }
+
+            // ..and EVEN MORE FRAMETIMES
+            for n in 0..32 {
+                let ptr_frametime_unknown = ptr_flipper.offset(0x2c4 + n * 0x4) as *mut f32;
+                std::ptr::write_volatile(ptr_frametime_unknown, frametime_truncated);
+            }
         }
     }
 }
 
 // FPS custom limit patch
-// This patch adjusts the target frame delta based on a custom set FPS limit.
-// It is only active while the FPS patch is enabled too, and uses the default value if it's set to 0 or less.
-// This does not allow you to go above the stock FPS limit. It is purely a QoL patch to improve glitch consistency, not an FPS unlocker.
 unsafe extern "win64" fn fps_custom_limit(registers: *mut Registers, _:usize)
 {
     unsafe
     {
+        // Flipper struct: Contains all the stuff we need
+        let ptr_flipper = (*registers).rax as *const u8;
+
+        // Frame limitting modes: 5 by default, >5 works for manual control
+        // "_alternative" might be used when a debug flag is enabled?
+        let ptr_frame_mode = ptr_flipper.offset(0x8) as *mut u32;
+        let ptr_frame_mode_alternative = ptr_flipper.offset(0xc) as *mut u32;
+        let mut frame_mode = 5;
+
         if DS3_FPS_PATCH_ENABLED && DS3_FPS_CUSTOM_LIMIT > 0.0f32
         {
-            let ptr_flipper = (*registers).rbx as *const u8; // Flipper struct - Contains all the stuff we need
+            // Unknown values set by the FPS limitting function in default mode
+            // Since the game doesn't set them with manual control, just do what the game would do by default
+            let ptr_unknown_0x14 = ptr_flipper.offset(0x14) as *mut u8;
+            let ptr_unknown_0x10 = ptr_flipper.offset(0x10) as *mut u32;
+            let ptr_unknown_0x270 = ptr_flipper.offset(0x270) as *mut u64;
 
-            let ptr_target_frame_delta = ptr_flipper.offset(0x18) as *mut f32; // Target frame delta - Set in a switch/case at the start
+            // Target frame delta (FPS limit)
+            let ptr_target_frame_delta = ptr_flipper.offset(0x18) as *mut f32;
+
 
             // Read the stock target frame delta and calculate the custom target frame delta
-            let target_frame_delta = std::ptr::read_volatile(ptr_target_frame_delta);
             let custom_target_frame_delta = 1.0f32 / DS3_FPS_CUSTOM_LIMIT;
 
-            // Make sure the custom target frame delta is higher than the stock one, in order to avoid going above the stock FPS limit
-            if custom_target_frame_delta > target_frame_delta
-            {
-                // Write values back
-                std::ptr::write_volatile(ptr_target_frame_delta, custom_target_frame_delta);
-            }
+            frame_mode = 99; // Anything >5 should work
+
+            // Set unknown values to what the game would set them to
+            std::ptr::write_volatile(ptr_unknown_0x14, 0);
+            std::ptr::write_volatile(ptr_unknown_0x10, 1);
+            std::ptr::write_volatile(ptr_unknown_0x270, 0);
+
+            // Set target frame delta
+            std::ptr::write_volatile(ptr_target_frame_delta, custom_target_frame_delta);
         }
+
+        // Set frame limitting mode
+        std::ptr::write_volatile(ptr_frame_mode, frame_mode);
+        std::ptr::write_volatile(ptr_frame_mode_alternative, frame_mode);
     }
+
 }
 
 // Frame advance patch
